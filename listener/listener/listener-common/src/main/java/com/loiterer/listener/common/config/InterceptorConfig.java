e37d467148b1b51ea09f462ed0028337881360d0
chenmt <2252540056@qq.com>
2020-10-29 00:33:45 +0800

    添加了认证拦截，编写了jwt,redis工具类

---
+++ listener/listener/listener-common/src/main/java/com/loiterer/listener/common/config/InterceptorConfig.java
@@ -0,0 +1,26 @@
+package com.loiterer.listener.common.config;
+
+import com.loiterer.listener.common.handler.AuthenticationInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 拦截器配置类
+ *
+ * @author cmt
+ * @date 2020/10/21
+ */
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private AuthenticationInterceptor authenticationInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor)
+                .excludePathPatterns("/user/login");
+    }
+}
