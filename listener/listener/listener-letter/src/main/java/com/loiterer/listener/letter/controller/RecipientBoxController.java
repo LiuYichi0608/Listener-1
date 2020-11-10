946b087239eec96e5be1101ed04462ef3a1260b7
LiuYichi0608 <1097336837@qq.com>
2020-11-10 15:14:03 +0800

    完成了收件箱信件展示、删除、更新阅读状态的功能接口

--- listener/listener/listener-letter/src/main/java/com/loiterer/listener/letter/controller/RecipientBoxController.java
+++ listener/listener/listener-letter/src/main/java/com/loiterer/listener/letter/controller/RecipientBoxController.java
@@ -1,13 +1,16 @@
 package com.loiterer.listener.letter.controller;
 
 
+import com.loiterer.listener.common.result.ResultEntity;
 import com.loiterer.listener.common.util.JwtUtil;
 
+import com.loiterer.listener.letter.model.vo.RecipientBoxVO;
 import com.loiterer.listener.letter.service.RecipientBoxService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * <p>
@@ -18,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020-11-08
  */
 @RestController
-@RequestMapping("/letter/recipient-box")
+@RequestMapping("/letter/recipientBox")
 public class RecipientBoxController {
 
     /**
@@ -40,6 +43,64 @@ public class RecipientBoxController {
     public RecipientBoxController(RecipientBoxService recipientBoxService, JwtUtil jwtUtil) {
         this.recipientBoxService = recipientBoxService;
         this.jwtUtil = jwtUtil;
+    }
+
+    /**
+     * 获取该用户所有自己收信的信件
+     * @param request 从request中获取openid
+     * @return 返回该用户收到的的所有未删除信件
+     */
+    @GetMapping("/getLetters")
+    public ResultEntity getAllRecipientLetters(HttpServletRequest request) {
+        // 1.使用jwtUtil从请求头获取openid
+        String openid = jwtUtil.getOpenid(request.getHeader("token"));
+
+        // 2.获取用户所有自己收到的未删除的信件
+        List<RecipientBoxVO> recipientBoxVOList = recipientBoxService.getALlRecipientLettersByOpenid(openid);
+
+        // 3.判断获取信件结果
+        if(recipientBoxVOList == null) {
+            // 3.1 获取信息失败, 返回失败信息
+            return ResultEntity.fail().message("获取信息失败");
+        } else {
+            // 3.2 获取信件成功, 返回信件信息
+            return ResultEntity.success().data("recipientBoxList",recipientBoxVOList);
+        }
+    }
+
+    @DeleteMapping("/deleteLetter/{id}")
+    public ResultEntity deleteRecipientLetter(@PathVariable("id") Integer id, HttpServletRequest request) {
+        // 1.使用jwtUtil从请求头获取openid
+        String openid = jwtUtil.getOpenid(request.getHeader("token"));
+
+        // 2.获取用户所有自己收到的未删除的信件
+        boolean flag = recipientBoxService.deleteRecipientLetter(id,openid);
+
+        if(!flag) {
+            // 3.1 删除信件失败
+            return ResultEntity.fail().message("删除信件失败");
+        } else {
+            // 3.2 删除信件成功
+            return ResultEntity.success().message("删除信件成功");
+        }
+    }
+
+    @PutMapping("/updateLetter/{id}")
+    public ResultEntity updateRecipientLetter(@PathVariable("id") Integer id, HttpServletRequest request) {
+        // 1.使用jwtUtil从请求头获取openid
+        String openid = jwtUtil.getOpenid(request.getHeader("token"));
+
+        // 2.获取用户所有自己收到的未删除的信件
+        boolean flag = recipientBoxService.updateRecipientLetter(id,openid);
+
+        // 3.判断更新信件结果
+        if(!flag) {
+            // 3.1 更新未读信件失败
+            return ResultEntity.fail().message("更新未读信件失败");
+        } else {
+            // 3.2 更新未读信件成功
+            return ResultEntity.success().message("更新未读信件成功");
+        }
     }
 }
 
